b7ab43e65b5ba3ae4541e97c39fb9a318c02d556
Preet mehra <143023174+ROYAL-JATT@users.noreply.github.com>
2025-04-13 00:46:43 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -62,8 +62,8 @@ async def resetpower(bot, message):
 async def vip1(bot,update):
     id = update.message.reply_to_message.text.split("/addpremium")
     user_id = id[1].replace(" ", "")
-    inlimit  = 21474836500
-    uploadlimit(int(user_id),21474836500)
+    inlimit  = 7861360572
+    uploadlimit(int(user_id),7861360572)
     usertype(int(user_id),"🪙 Basic")
     addpre(int(user_id))
     await update.message.edit("Added Successfully To Premium Upload Limit 20 GB")
@@ -75,8 +75,8 @@ async def vip1(bot,update):
 async def vip2(bot,update):
     id = update.message.reply_to_message.text.split("/addpremium")
     user_id = id[1].replace(" ", "")
-    inlimit = 53687091200
-    uploadlimit(int(user_id),53687091200)
+    inlimit = 7861360572
+    uploadlimit(int(user_id),7861360572)
     usertype(int(user_id),"⚡ Standard")
     addpre(int(user_id))
     await update.message.edit("Added Successfully To Premium Upload Limit 50 GB")
@@ -88,8 +88,8 @@ async def vip2(bot,update):
 async def vip3(bot,update):
     id = update.message.reply_to_message.text.split("/addpremium")
     user_id = id[1].replace(" ", "")
-    inlimit = 107374182400
-    uploadlimit(int(user_id),107374182400)
+    inlimit = 7861360572
+    uploadlimit(int(user_id),7861360572)
     usertype(int(user_id),"💎 Pro")
     addpre(int(user_id))
     await update.message.edit("Added Successfully To Premium Upload Limit 100 GB")
@@ -103,8 +103,8 @@ async def vip3(bot,update):
 async def cp1(bot,update):
     id = update.message.reply_to_message.text.split("/ceasepower")
     user_id = id[1].replace(" ", "")
-    inlimit  = 2147483652
-    uploadlimit(int(user_id),2147483652)
+    inlimit  = 7861360572
+    uploadlimit(int(user_id),7861360572)
     usertype(int(user_id),"⚠️ Account Downgraded")
     addpre(int(user_id))
     await update.message.edit("Added Successfully To Upload Limit 2GB")
@@ -130,8 +130,8 @@ async def cp2(bot,update):
 async def dft(bot,update):
     id = update.message.reply_to_message.text.split("/resetpower")
     user_id = id[1].replace(" ", "")
-    inlimit = 2147483652
-    uploadlimit(int(user_id),2147483652)
+    inlimit = 7861360572
+    uploadlimit(int(user_id),7861360572)
     usertype(int(user_id),"🆓 Free")
     addpre(int(user_id))
     await update.message.edit("Daily Data Limit Has Been Reset Successfully.\n\nThis Account Has Default 2GB Remaining Capacity")
